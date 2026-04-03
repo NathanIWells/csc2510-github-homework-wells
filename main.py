ccd4e5827cb6c7262dcd769ca3407f5ee33f62a7
NathanIWells <niwells42@tntech.edu>
2026-04-03 12:06:36 -0500

    Added funcWells and made changes to fightClub

--- main.py
+++ main.py
@@ -19,8 +19,17 @@ def funcVandergriff():
                     """)
 
 
+
+def funcWells():
+    # Rats
+    for i in range(4):
+      print("Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy.")
+    
 def fightClub(): 
-    print("Guys, we're not supposed to talk about this. ")
+    print("ARE YOU READY TO RUMBLE???????")
+    print("THROW THIS MAN INTO THE RUBBER ROOM")
+    print("RELEASE THE RATS")
+    funcWells()
 
 if __name__ == "__main__":
     funcVandergriff()
